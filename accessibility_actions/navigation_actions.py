4815acc024668ecec8f5fe30d7748cd4cbbea520
Willian Massami Watanabe <watinha@gmail.com>
2011-07-18 01:29:20 -0300

    Adds max number of tab key presses as a constant

--- accessibility_actions/navigation_actions.py
+++ accessibility_actions/navigation_actions.py
@@ -6,10 +6,11 @@ class TabNavigationAction(ActionBase):
     regex = r'^(And )?I tab navigate to [\"](?P<element_text>[^"]+)[\"] (?P<element_type><element selector>)$'
     NOT_FOUND_ELEMENT = 'element not found'
     MAX_TAB_PRESSES_EXCEEDED = 'exceed max number of tab keys pressed'
+    MAX_NUMBER_OF_TAB_KEYS = 30
 
     def execute(self, context, element_type, element_text):
         js_loader = JsCodeLoader()
-        js_code = js_loader.load('tab_navigation.js', element_text)
+        js_code = js_loader.load('tab_navigation.js', element_text, self.MAX_NUMBER_OF_TAB_KEYS)
 
         result = context.browser_driver.exec_js(js_code)
         if (result == self.NOT_FOUND_ELEMENT):
