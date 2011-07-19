4f061287f61b20dad59e4837f4bf3cebc3d1226d
Willian Massami Watanabe <watinha@gmail.com>
2011-07-19 00:44:41 -0300

    Fix in navigation actions considering the tab_limit argument

--- tests/test_navigation_actions.py
+++ tests/test_navigation_actions.py
@@ -22,10 +22,33 @@ class TabNavigationActionTest(unittest.TestCase):
         with patch.object(JsCodeLoader, 'load') as load_mock:
             load_mock.return_value = js_code_dummy
             tab_navigation = TabNavigationAction()
-            tab_navigation.execute(context_mock, 'element', element_text_dummy)
+            tab_navigation.execute(context_mock, 'element', element_text_dummy, False)
 
         self.assertTrue(load_mock.called)
         load_mock.assert_called_with('tab_navigation.js', element_text_dummy, '30')
+
+        self.assertTrue(context_mock.browser_driver.exec_js.called)
+        context_mock.browser_driver.exec_js.assert_called_with(js_code_dummy)
+
+    def test_execute_function_should_exec_js_and_return_searched_element_with_tab_limit_argument(self):
+        element_text_dummy = 'a link to look for'
+        js_code_dummy = """
+        function tab_navigation(text){
+            console.log('unit_testing:' + text);
+        }
+        result = tab_navigation("a link to look for", "2");
+        """
+
+        context_mock = Mock()
+        context_mock.browser_driver.exec_js.return_value = element_text_dummy
+
+        with patch.object(JsCodeLoader, 'load') as load_mock:
+            load_mock.return_value = js_code_dummy
+            tab_navigation = TabNavigationAction()
+            tab_navigation.execute(context_mock, 'element', element_text_dummy, '2')
+
+        self.assertTrue(load_mock.called)
+        load_mock.assert_called_with('tab_navigation.js', element_text_dummy, '2')
 
         self.assertTrue(context_mock.browser_driver.exec_js.called)
         context_mock.browser_driver.exec_js.assert_called_with(js_code_dummy)
@@ -46,7 +69,7 @@ class TabNavigationActionTest(unittest.TestCase):
             load_mock.return_value = js_code_dummy
             tab_navigation = TabNavigationAction()
             try:
-                tab_navigation.execute(context_mock, 'element', element_text_dummy)
+                tab_navigation.execute(context_mock, 'element', element_text_dummy, False)
                 self.fail('if "element not found" received should raise a failed scenario exception')
             except ActionFailedError:
                 self.assertTrue(True)
@@ -68,7 +91,7 @@ class TabNavigationActionTest(unittest.TestCase):
             load_mock.return_value = js_code_dummy
             tab_navigation = TabNavigationAction()
             try:
-                tab_navigation.execute(context_mock, 'element', element_text_dummy)
+                tab_navigation.execute(context_mock, 'element', element_text_dummy, False)
                 self.fail('if "exceed max number of tab keys pressed" received should raise a failed scenario exception')
             except ActionFailedError:
                 self.assertTrue(True)
