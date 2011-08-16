852483334ed897f9e7146088511aa4ffd1201030
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2011-08-15 23:25:51 -0300

    Includes form submit functionality to press enter action

--- js_codes/activate_elements.js
+++ js_codes/activate_elements.js
@@ -1,8 +1,25 @@
 function ActivateElements() {
 }
+ActivateElements.prototype.constants = {
+    FORM_ELEMENT_NOT_FOUND: "parent form not found"
+};
 ActivateElements.prototype.execute = function () {
-    var click_event = current_document.createEvent("MouseEvent");
-    click_event.initMouseEvent("click", true, true, current_window, 12, 12, 12, 12, 12, false, false, false, false, 0, null);
-    current_document.activeElement.dispatchEvent(click_event);
-    return "link activated";
+    var active_element = current_document.activeElement,
+        click_event = current_document.createEvent("MouseEvent");
+
+    if (active_element.tagName == "A") {
+        click_event.initMouseEvent("click", true, true, current_window, 12, 12, 12, 12, 12, false, false, false, false, 0, null);
+        current_document.activeElement.dispatchEvent(click_event);
+        return "link activated";
+    } else if (active_element.tagName == "INPUT") {
+        var form_parent = this._search_form_parent(active_element);
+        form_parent.submit();
+    }
+}
+ActivateElements.prototype._search_form_parent = function (input_element) {
+    if (input_element.parentNode.tagName == "FORM")
+        return input_element.parentNode;
+    if (input_element.parentNode.tagName == "BODY")
+        return this.constants.FORM_ELEMENT_NOT_FOUND;
+    return this._search_form_parent(input_element.parentNode);
 }
--- js_codes_tests/test_activate_elements.js
+++ js_codes_tests/test_activate_elements.js
@@ -35,4 +35,37 @@ ActivateElementsTest.prototype.run_test = function () {
         document.querySelectorAll("#link1")[0].removeEventListener("click", click_event);
     });
 
+    test("test form activation should follow form action attribute", function () {
+        var previous_submit = document.querySelectorAll("form")[0].submit;
+        document.querySelectorAll("form")[0].submit = function () {
+            ok(true, "form submitted ok");
+        }
+        document.querySelectorAll("form > input")[0].value = "watinha";
+        document.querySelectorAll("form > input")[0].focus();
+
+        var action = new ActivateElements();
+        var result = action.execute();
+
+        expect(1);
+
+        // avoid tests interference
+        document.querySelectorAll("form")[0].submit = previous_submit;
+    });
+
+    test("test _search_form_parent function gets the parent form element", function () {
+        var fixture_input = document.createElement("input"),
+            fixture_div = document.createElement("div"),
+            fixture_span = document.createElement("span"),
+            fixture_form = document.createElement("form");
+
+        fixture_form.appendChild(fixture_div);
+        fixture_div.appendChild(fixture_span);
+        fixture_span.appendChild(fixture_input);
+
+        var action = new ActivateElements(),
+            parent_form = action._search_form_parent(fixture_input);
+
+        equal(parent_form, fixture_form);
+    });
+
 }
